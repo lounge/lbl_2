cd414c44a175554045e1b6ed560e008f761f634e
lounge <fredlunden@gmail.com>
2023-11-18 17:31:59 +0100

    split fe & be

--- app.py
+++
@@ -1,8 +0,0 @@
-from spin_http import Response
-
-
-def handle_request(request):
-
-    return Response(200,
-                    {"content-type": "text/plain"},
-                    bytes(f"jiddi jadda osv...", "utf-8"))
