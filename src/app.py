31f6b95b868b78eea3a75fc0b57b7e0d779ca8d6
lounge <fredlunden@gmail.com>
2024-04-12 15:53:52 +0200

    Testing sqlite3

--- src/app.py
+++ src/app.py
@@ -1,8 +1,26 @@
-from spin_http import Response
+from spin_sdk import http, sqlite
+from spin_sdk.http import Request, Response
+from spin_sdk.sqlite import Value_Integer, Value_Text
+from datetime import date
 
 
-def handle_request(request):
+# def handle_request(request):
 
-    return Response(200,
-                    {"content-type": "text/plain"},
-                    bytes(f"jiddi jadda BE 0.1.0 osv...", "utf-8"))
+#     datetime.date().
+#     return Response(200,
+#                     {"content-type": "text/plain"},
+#                     bytes(f"jiddi jadda BE 0.1.0 osv...", "utf-8"))
+
+
+class IncomingHandler(http.IncomingHandler):
+    def handle_request(self, request: Request) -> Response:
+            print(date.today())
+
+            with sqlite.open_default() as db:
+                print("Connected to SQLite")
+                db.execute("INSERT INTO todos (description, due) VALUES (?, ?)", [Value_Text("test"), Value_Text("2024-04-11")])
+                            
+            return Response(
+                200,
+                {"content-type": "text/plain"},
+                bytes(f"jiddi jadda BE 0.2.0 osv...", "utf-8"))
